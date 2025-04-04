76c43c976cd1de6da750d7adc559fc29cb871b5c
Yash Goyal <yashgo0018@gmail.com>
2025-04-04 16:54:14 +0300

    created ignition module for the fund contract

---
+++ packages/hardhat/ignition/modules/Fund.ts
@@ -0,0 +1,32 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const FundModule = buildModule("FundModule", (m) => {
+  const name = m.getParameter("name", "Fund");
+  const symbol = m.getParameter("symbol", "FUND");
+
+  // USDC address on Base
+  const usdcAddress = m.getParameter(
+    "usdcAddress",
+    "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"
+  );
+
+  // Uniswap V2 Factory address on Base
+  const uniswapFactoryAddress = m.getParameter(
+    "uniswapFactoryAddress",
+    "0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6"
+  );
+
+  const fund = m.contract("Fund", [
+    name,
+    symbol,
+    usdcAddress,
+    uniswapFactoryAddress,
+  ]);
+
+  return { fund };
+});
+
+export default FundModule;
