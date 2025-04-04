af6c29b65adfbf0f48f879dc3985beb0e230e0a8
Yash Goyal <yashgo0018@gmail.com>
2025-04-04 15:48:46 +0300

    added base support in hardhat

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -1,8 +1,26 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import "dotenv/config";
+import { z } from "zod";
+
+const envSchema = z.object({
+  ALCHEMY_API_KEY: z.string(),
+  PRIVATE_KEY: z.string().startsWith("0x"),
+});
+
+const { ALCHEMY_API_KEY, PRIVATE_KEY } = envSchema.parse(process.env);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    base: {
+      url: `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
